ad7c27d0f435219f631697a53a3f49b15d7ef4a6
GabriHR <ghernrod@myuax.com>
2024-04-16 22:49:00 +0200

    11

--- src/main/java/Herramienta_Analisis_Numerico/Sumatoria_Listado_Numeros.java
+++ src/main/java/Herramienta_Analisis_Numerico/Sumatoria_Listado_Numeros.java
@@ -13,44 +13,4 @@ public class Sumatoria_Listado_Numeros {
         }
     }
 
-    public List<Integer> listarNumeros(int inicio, int fin) {
-        if (inicio > fin) {
-            return new ArrayList<>();
-        } else {
-            List<Integer> lista = listarNumeros(inicio, fin - 1);
-            lista.add(fin);
-            return lista;
-        }
-    }
-
-    public List<Integer> ordenarNumeros(int inicio, int fin) {
-        List<Integer> numeros = listarNumeros(inicio, fin);
-        ordenarNumerosRecursivo(numeros, 0, numeros.size() - 1);
-        return numeros;
-    }
-
-    private void ordenarNumerosRecursivo(List<Integer> numeros, int inicio, int fin) {
-        if (inicio < fin) {
-            int pivotIndex = partition(numeros, inicio, fin);
-            ordenarNumerosRecursivo(numeros, inicio, pivotIndex - 1);
-            ordenarNumerosRecursivo(numeros, pivotIndex + 1, fin);
-        }
-    }
-
-    private int partition(List<Integer> numeros, int inicio, int fin) {
-        int pivot = numeros.get(fin);
-        int i = inicio - 1;
-        for (int j = inicio; j < fin; j++) {
-            if (numeros.get(j) < pivot) {
-                i++;
-                int temp = numeros.get(i);
-                numeros.set(i, numeros.get(j));
-                numeros.set(j, temp);
-            }
-        }
-        int temp = numeros.get(i + 1);
-        numeros.set(i + 1, numeros.get(fin));
-        numeros.set(fin, temp);
-        return i + 1;
-    }
 }
