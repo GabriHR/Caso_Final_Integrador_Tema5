f39a88140e303e7b0417da00ff281b77d3444939
GabriHR <ghernrod@myuax.com>
2024-04-16 22:51:16 +0200

    14

--- src/main/java/Herramienta_Analisis_Numerico/Sumatoria_Listado_Numeros.java
+++ src/main/java/Herramienta_Analisis_Numerico/Sumatoria_Listado_Numeros.java
@@ -1,5 +1,7 @@
 package Herramienta_Analisis_Numerico;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Sumatoria_Listado_Numeros {
 
@@ -8,6 +10,16 @@ public class Sumatoria_Listado_Numeros {
             return 0;
         } else {
             return n + calcularSumatoria(n - 1);
+        }
+    }
+
+    public List<Integer> listarNumeros(int inicio, int fin) {
+        if (inicio > fin) {
+            return new ArrayList<>();
+        } else {
+            List<Integer> lista = listarNumeros(inicio, fin - 1);
+            lista.add(fin);
+            return lista;
         }
     }
 
