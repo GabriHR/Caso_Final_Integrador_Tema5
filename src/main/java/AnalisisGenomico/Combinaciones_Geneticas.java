543514a816622dcfc1d76ce0c7051a82ae8cbc65
GabriHR <ghernrod@myuax.com>
2024-04-16 22:44:34 +0200

    7

--- src/main/java/AnalisisGenomico/Combinaciones_Geneticas.java
+++ src/main/java/AnalisisGenomico/Combinaciones_Geneticas.java
@@ -10,4 +10,20 @@ public class Combinaciones_Geneticas {
         }
     }
 
+    public static long calcularCombinacionesIterativo(int n, int k) {
+        long C[][] = new long[n + 1][k + 1];
+        int i, j;
+
+        for (i = 0; i <= n; i++) {
+            for (j = 0; j <= Math.min(i, k); j++) {
+                if (j == 0 || j == i) {
+                    C[i][j] = 1;
+                } else {
+                    C[i][j] = C[i - 1][j - 1] + C[i - 1][j];
+                }
+            }
+        }
+
+        return C[n][k];
+    }
 }
