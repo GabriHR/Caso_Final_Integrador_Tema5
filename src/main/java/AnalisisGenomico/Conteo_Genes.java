693cd464f0b0d700199049bcaa509910ed5a353b
GabriHR <ghernrod@myuax.com>
2024-04-16 22:43:22 +0200

    4

---
+++ src/main/java/AnalisisGenomico/Conteo_Genes.java
@@ -0,0 +1,11 @@
+package AnalisisGenomico;
+
+public class Conteo_Genes {
+
+    public static int countGenes(String dna, int index) {
+        if (index > dna.length() - 3) {
+            return 0;
+        }
+
+    }
+}
