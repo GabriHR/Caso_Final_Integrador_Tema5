aadcf7bf04b7802997501966770ba157d1bcd86d
GabriHR <ghernrod@myuax.com>
2024-04-17 08:09:20 +0200

    35

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.*;
+
 
 public class Main {
 
@@ -15,4 +17,45 @@ public class Main {
         frame.setVisible(true);
     }
 
+    private static void placeComponents(JPanel panel) {
 
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        JLabel titleLabel = new JLabel("INTERFAZ DE USUARIO PARA LOS INVESTIGADORES");
+        titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(titleLabel);
+
+        panel.add(Box.createRigidArea(new Dimension(0, 20))); // Agrega espacio entre los componentes
+
+        String[] options = {
+                "Conteo de genes",
+                "Combinaciones genéticas recursivas",
+                "Combinaciones genéticas iterativas",
+                "Calcular potencias y encontrar máximo",
+                "Calcular sumatoria",
+                "Ordenar documento (A-Z)",
+                "Busqueda de palabras",
+                "Agregar fecha",
+                "Listar fechas",
+                "Algoritmo quicksort (ascendente)",
+                "Algoritmo quicksort (descendente)"
+        };
+
+        JComboBox<String> comboBox = new JComboBox<>(options);
+        comboBox.setMaximumSize(comboBox.getPreferredSize()); // Para evitar que el JComboBox se estire para llenar todo el ancho del panel
+        comboBox.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(comboBox);
+
+        panel.add(Box.createRigidArea(new Dimension(0, 20))); // Agrega espacio entre los componentes
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER)); // Cambiamos a FlowLayout con alineación central
+        JButton confirmButton = new JButton("Confirmar");
+        buttonPanel.add(confirmButton);
+        JButton exitButton = new JButton("Salir");
+        buttonPanel.add(exitButton);
+        panel.add(buttonPanel);
+
+
+    }
+}
