216e888e24f4232cbb560df5bbb0a1ff7902d464
GabriHR <ghernrod@myuax.com>
2024-04-17 08:00:19 +0200

    27

--- src/main/java/Gestion_Informacion_Cientifica/Gestion_Fechas.java
+++ src/main/java/Gestion_Informacion_Cientifica/Gestion_Fechas.java
@@ -12,4 +12,37 @@ public class Gestion_Fechas {
         fechas.add(fecha);
     }
 
+    public List<LocalDate> listarFechas() {
+        fechas = mergeSort(fechas);
+        return fechas;
+    }
+
+    private List<LocalDate> mergeSort(List<LocalDate> fechas) {
+        if (fechas.size() <= 1) {
+            return fechas;
+        }
+        int mid = fechas.size() / 2;
+        List<LocalDate> left = mergeSort(new ArrayList<>(fechas.subList(0, mid)));
+        List<LocalDate> right = mergeSort(new ArrayList<>(fechas.subList(mid, fechas.size())));
+        return merge(left, right);
+    }
+
+    private List<LocalDate> merge(List<LocalDate> left, List<LocalDate> right) {
+        List<LocalDate> merged = new ArrayList<>();
+        int i = 0, j = 0;
+        while (i < left.size() && j < right.size()) {
+            if (left.get(i).compareTo(right.get(j)) <= 0) {
+                merged.add(left.get(i++));
+            } else {
+                merged.add(right.get(j++));
+            }
+        }
+        while (i < left.size()) {
+            merged.add(left.get(i++));
+        }
+        while (j < right.size()) {
+            merged.add(right.get(j++));
+        }
+        return merged;
+    }
 }
